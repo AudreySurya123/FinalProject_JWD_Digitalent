0e362ded86bf18b87bcff37d54c7336ccaf20eb8
surya <audreysurya16@gmail.com>
2023-07-27 15:49:34 +0700

    first commit

---
+++ clear.sql
@@ -0,0 +1,199 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 26 Jul 2023 pada 08.02
+-- Versi server: 10.4.28-MariaDB
+-- Versi PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `laundry`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_jenis`
+--
+
+CREATE TABLE `tb_jenis` (
+  `kd_jenis` int(11) NOT NULL,
+  `jenis_laundry` varchar(100) NOT NULL,
+  `lama_proses` int(11) NOT NULL,
+  `tarif` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_laporan`
+--
+
+CREATE TABLE `tb_laporan` (
+  `id_laporan` int(11) NOT NULL,
+  `tgl_laporan` date NOT NULL,
+  `ket_laporan` int(1) NOT NULL,
+  `catatan` text NOT NULL,
+  `id_laundry` char(10) DEFAULT NULL,
+  `pemasukan` int(11) NOT NULL,
+  `id_pengeluaran` char(10) DEFAULT NULL,
+  `pengeluaran` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_laundry`
+--
+
+CREATE TABLE `tb_laundry` (
+  `id_laundry` char(10) NOT NULL,
+  `pelangganid` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `kd_jenis` char(6) NOT NULL,
+  `tgl_terima` date NOT NULL,
+  `tgl_selesai` date NOT NULL,
+  `jml_kilo` int(11) NOT NULL,
+  `catatan` text NOT NULL,
+  `totalbayar` int(11) NOT NULL,
+  `status_pembayaran` int(1) NOT NULL,
+  `status_pengambilan` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_pelanggan`
+--
+
+CREATE TABLE `tb_pelanggan` (
+  `pelangganid` int(11) NOT NULL,
+  `pelanggannama` varchar(150) NOT NULL,
+  `pelangganjk` varchar(15) NOT NULL,
+  `pelangganalamat` text NOT NULL,
+  `pelanggantelp` varchar(20) NOT NULL,
+  `pelangganfoto` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_pengeluaran`
+--
+
+CREATE TABLE `tb_pengeluaran` (
+  `id_pengeluaran` char(10) NOT NULL,
+  `tgl_pengeluaran` date NOT NULL,
+  `catatan` text NOT NULL,
+  `pengeluaran` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_users`
+--
+
+CREATE TABLE `tb_users` (
+  `userid` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `userpass` varchar(100) NOT NULL,
+  `nama` varchar(150) NOT NULL,
+  `jk` varchar(15) NOT NULL,
+  `alamat` text NOT NULL,
+  `usertelp` varchar(20) NOT NULL,
+  `userfoto` varchar(50) DEFAULT NULL,
+  `level` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `tb_users`
+--
+
+INSERT INTO `tb_users` (`userid`, `username`, `userpass`, `nama`, `jk`, `alamat`, `usertelp`, `userfoto`, `level`) VALUES
+(1, 'admin', '$2y$10$tZGYTUMpIo55gRB4VWS.N.8oAZoscSeZw8fvWh9gzSeuWpF7cHXhq', 'sevyra', 'Perempuan', 'madiun', '09876543', '64bfe49ed4126.png', 'admin'),
+(2, 'kasir', '$2y$10$8koBFAeaG0hFdqFjcKSYTe7qUdbfGGGIgmxPr8E0Sbrywcn/MIzXa', 'surya', 'Laki - laki', 'madiun', '0888655', NULL, 'kasir');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tb_jenis`
+--
+ALTER TABLE `tb_jenis`
+  ADD PRIMARY KEY (`kd_jenis`);
+
+--
+-- Indeks untuk tabel `tb_laporan`
+--
+ALTER TABLE `tb_laporan`
+  ADD PRIMARY KEY (`id_laporan`);
+
+--
+-- Indeks untuk tabel `tb_laundry`
+--
+ALTER TABLE `tb_laundry`
+  ADD PRIMARY KEY (`id_laundry`);
+
+--
+-- Indeks untuk tabel `tb_pelanggan`
+--
+ALTER TABLE `tb_pelanggan`
+  ADD PRIMARY KEY (`pelangganid`);
+
+--
+-- Indeks untuk tabel `tb_pengeluaran`
+--
+ALTER TABLE `tb_pengeluaran`
+  ADD PRIMARY KEY (`id_pengeluaran`);
+
+--
+-- Indeks untuk tabel `tb_users`
+--
+ALTER TABLE `tb_users`
+  ADD PRIMARY KEY (`userid`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_jenis`
+--
+ALTER TABLE `tb_jenis`
+  MODIFY `kd_jenis` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_laporan`
+--
+ALTER TABLE `tb_laporan`
+  MODIFY `id_laporan` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_pelanggan`
+--
+ALTER TABLE `tb_pelanggan`
+  MODIFY `pelangganid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_users`
+--
+ALTER TABLE `tb_users`
+  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
